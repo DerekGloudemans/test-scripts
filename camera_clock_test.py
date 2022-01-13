60c70a520019c8398d088d984bd4ddc2cc5b8f10
Derek Gloudemans <35013478+DerekGloudemans@users.noreply.github.com>
2022-01-13 14:57:40 -0600

    Create camera_clock_test.py

---
+++ camera_clock_test.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from 
+
+%## global settings
+coarse_rps = 1
+fine_rps = 8
+
+
+%## Load video file
+
+
+%## using first frame, select center and radius of coarse and fine wheel
+
+%## for each frame, extract timestamp
+
+%## for each frame, detect edges of thresholded and masked image (mask with white)
+
+def extract_analog_time(frame,center1,center2,radius1,radius2):
+  pass
+
+def extract_timestamp_time():
+  pass
